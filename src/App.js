42714b3bc71cd4754d6010938af146948b2cb596
Mobina <esmaeli@ossl.ir>
2022-12-06 11:48:07 +0330

    navbar&footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
-
+import './App.css';
+import React,{Component}  from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import {Route , Routes} from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-     Name 
-    </div>
-  );
+    return (
+        <>
+
+            <Navbar />
+            <Routes>
+            </Routes>
+            <Footer />
+
+        </>
+    )
 }
 
 export default App;
