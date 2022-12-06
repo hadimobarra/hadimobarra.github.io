7d4d5311680a16b92881cde05ce469fede0417b0
Mobina <esmaeli@ossl.ir>
2022-12-06 12:10:48 +0330

    edited navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import './App.css';
 import React,{Component}  from "react";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import {Route , Routes} from "react-router-dom";
 function App() {
     return (
         <>
-
-            <Navbar />
+            {/*Navbar*/}
+            {/*<Navbar />*/}
             <Routes>
             </Routes>
-            <Footer />
+            {/*Footer*/}
+            {/*<Footer />*/}
 
         </>
     )
