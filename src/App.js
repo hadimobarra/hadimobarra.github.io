3c95e3a34a55ab36677b73eb6bbe3afd8e122bb0
hadimobarra <hadimobarra@gmail.com>
2022-12-21 09:48:05 +0330

    debug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IntroPage from './pages/introPage/introPage';
-import FrontPage from './pages/frontPage/frontPage';
-import UiPage from './pages/uiPage/uiPage';
-import BackPage from './pages/backPage/backPage';
-import DataPage from './pages/dataPage/dataPage';
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import IntroPage from './pages/introPage/introPage';
+//import FrontPage from './pages/frontPage/frontPage';
+//import UiPage from './pages/uiPage/uiPage';
+//import BackPage from './pages/backPage/backPage';
+//import DataPage from './pages/dataPage/dataPage';
 
 	
 function App() {
     return (
-		<>
-			<Routes>
+		<><p>hello from hadi</p>
+			{/*<Routes>
 				<Route exact path="/" element={<IntroPage />} />
 				<Route path="/ui" element={<UiPage />} />
 				<Route path="/front" element={<FrontPage />} />
 				<Route path="/back" element={<BackPage />} />
 				<Route path="/data" element={<DataPage />} />
-			</Routes>
+			</Routes> */}
 		</> 
     )
 }
